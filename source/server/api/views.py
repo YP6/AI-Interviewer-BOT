ea95ef089b0c419a5d8d5ba86b8d6b4235bb1bd8
Youssef Khaled <youssefkfarouk@gmail.com>
2023-03-29 00:31:58 +0200

    feat: added the ability to add interviews, questions, and answers using the API

--- source/server/api/views.py
+++ source/server/api/views.py
@@ -6,10 +6,8 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required, user_passes_test
 from .serializers import *
 from ..models import yp6AuthenticationToken
-from ..AUTH import CheckAuthorization,Hash,RemoveAuthorization
+from ..AUTH import CheckAuthorization, Hash, RemoveAuthorization
 from .decorators import *
-
-
 
 
 @api_view(['POST'])
@@ -20,23 +18,23 @@ def LoginUser(request):
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
-    
-        return Response({'OK 200' : 'Sucess', 'detatil':'Authorized'}, status= status.HTTP_200_OK)
+
+        return Response({'OK 200': 'Sucess', 'detatil': 'Authorized'}, status=status.HTTP_200_OK)
     else:
-        return Response({'Error 400' : 'Bad Request','detatil':'Wrong Credentials'}, status=status.HTTP_400_BAD_REQUEST)
-    
+        return Response({'Error 400': 'Bad Request', 'detatil': 'Wrong Credentials'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['POST'])
 @IsAuthenticated
 def LogoutUser(request):
-    respones = Response({'OK 200':'Success', 'detail':'Logged Out'}, status=status.HTTP_200_OK)
+    respones = Response({'OK 200': 'Success', 'detail': 'Logged Out'}, status=status.HTTP_200_OK)
     respones.delete_cookie('csrftoken')
     if not RemoveAuthorization(request):
         return Response({'Error 400': 'Bad Request'}, status=status.HTTP_400_BAD_REQUEST)
     logout(request)
-    
-    return respones
 
+    return respones
 
 
 @api_view(['GET'])
@@ -44,32 +42,32 @@ def LogoutUser(request):
 def OptainAuthToken(request):
     try:
         token = yp6AuthenticationToken()
-        token.userID = User.objects.get(username = request.user.username)
+        token.userID = User.objects.get(username=request.user.username)
         dt = datetime.datetime.now()
-        tokenSTR = Hash(str(request.user.id)+ str(dt))
+        tokenSTR = Hash(str(request.user.id) + str(dt))
         token.token = tokenSTR
         try:
             token.macAddress = request.headers['MAC']
         except:
-            return Response({'Error 400': 'Bad Request', 'detatil': 'Mac Address is not Provided'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'Error 400': 'Bad Request', 'detatil': 'Mac Address is not Provided'},
+                            status=status.HTTP_400_BAD_REQUEST)
         token.save()
     except:
-        return Response({'Error 400': 'Bad Request', 'detail':'Unauthorized User'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'Error 400': 'Bad Request', 'detail': 'Unauthorized User'}, status=status.HTTP_400_BAD_REQUEST)
 
     respones = Response(tokenSTR)
     respones.status_code = 200
     return respones
 
 
-
-
 superUserTest = user_passes_test(lambda u: u.is_staff, login_url='../')
+
 
 @api_view(['GET'])
 def getAPIRoutes(request):
     routes = {
         'api/token': 'GET',
-        'api/account/login': 'POST', 
+        'api/account/login': 'POST',
         'api/account/logout': 'POST',
         'api/account/register': 'POST',
         'api/account/profile': 'GET',
@@ -94,14 +92,14 @@ def RegisterUser(request):
     try:
         User.register(**request.data)
     except Exception as err:
-        return Response({'Error 400': 'Bad Request' , 'detail': str(err)}, status=status.HTTP_400_BAD_REQUEST)
-    
+        return Response({'Error 400': 'Bad Request', 'detail': str(err)}, status=status.HTTP_400_BAD_REQUEST)
+
     username = request.data['username']
     password = request.data['password']
     user = authenticate(request, username=username, password=password)
     login(request, user=user)
-    return Response({'OK 200':'Success','detail':'Account Created Successfully'}, status=status.HTTP_200_OK)
-   
+    return Response({'OK 200': 'Success', 'detail': 'Account Created Successfully'}, status=status.HTTP_200_OK)
+
 
 @api_view(['GET'])
 def GetAccountTypes(request):
@@ -117,7 +115,8 @@ def CurrentProfile(request):
     if user.is_valid:
         return Response(user.data)
     else:
-        return Response({'Error 500' :  'Internal Server Error','detail': 'Can\'t retrieve your profile'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({'Error 500': 'Internal Server Error', 'detail': 'Can\'t retrieve your profile'},
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @api_view(['GET'])
@@ -127,6 +126,7 @@ def GetInterviews(request):
     serializedData = InterviewSerializer(interviews, many=True)
     return Response(serializedData.data)
 
+
 @api_view(['POST'])
 @IsAuthenticated
 def EditAccount(request):
@@ -134,16 +134,18 @@ def EditAccount(request):
     try:
         user.edit(**request.data)
     except Exception as err:
-        return Response({"Error 409":"Conflict In Database", "detail" : str(err)}, status=status.HTTP_409_CONFLICT)
-    return Response({"OK 200":"Account Updated Successfuly"}, status=status.HTTP_200_OK)
+        return Response({"Error 409": "Conflict In Database", "detail": str(err)}, status=status.HTTP_409_CONFLICT)
+    return Response({"OK 200": "Account Updated Successfuly"}, status=status.HTTP_200_OK)
+
 
 @api_view(['GET'])
 @IsAuthenticated
 def GetInterviewAttendances(request):
     userId = request.user.id
-    interviewsAttendances = InterviewAttendance.objects.filter(userID = userId)
+    interviewsAttendances = InterviewAttendance.objects.filter(userID=userId)
     serializedData = InterviewAttendanceSerializer(interviewsAttendances, many=True)
     return Response(serializedData.data)
+
 
 @api_view(['GET'])
 @IsAuthenticated
@@ -152,10 +154,11 @@ def GetInterviewsTopics(request):
     serializedData = TopicSerializer(topics, many=True)
     return Response(serializedData.data)
 
+
 @api_view(['GET'])
 @IsAuthenticated
 def GetInterview(request):
-    ret=[]
+    ret = []
     interviewID = request.data['id']
     interview = Interview.objects.get(id=interviewID)
     data = InterviewSerializer(interview).data
@@ -163,43 +166,56 @@ def GetInterview(request):
     try:
         questionsIDs = InterviewQuestion.objects.filter(interviewID=interviewID)
     except:
-        return Response({"Error 404":"No Questions Found", 
-        "detail":"Interview Questions are deleted or corrupted please contact the interviewer"}
-        , status=status.HTTP_404_NOT_FOUND)
+        return Response({"Error 404": "No Questions Found",
+                         "detail": "Interview Questions are deleted or corrupted please contact the interviewer"}
+                        , status=status.HTTP_404_NOT_FOUND)
     for q in questionsIDs:
         try:
             question = Question.objects.get(question=q.questionID)
             serializedQuestion = QuestionSerializer(question, many=False)
             ret.append(serializedQuestion.data)
         except:
-            return Response({"Error 404":"Some Questions are not found", 
-            "detail":"Some Interview Questions are deleted please contact the interviewer"}
-            , status=status.HTTP_404_NOT_FOUND)
+            return Response({"Error 404": "Some Questions are not found",
+                             "detail": "Some Interview Questions are deleted please contact the interviewer"}
+                            , status=status.HTTP_404_NOT_FOUND)
     return Response(ret, status=status.HTTP_200_OK)
+
 
 @api_view(['POST'])
 @IsAuthenticated
 @CanCreateInterview
 @CanCreateQuestion
 def AddInterview(request):
-    interview = Interview.add(request.user, **request.data)
+    if 'answers' in request.data['questions'][0].keys():
+        interview = Interview.add(request.user, **request.data)
+        if interview == None:
+            return Response({"Error 409": "Database Conflict", "detail": "Interview Already Exists"}, status=status.HTTP_409_CONFLICT)
+    else:
+        return Response({"Error 400": "Bad Request", "detail": "Can't Add interview without questions and answers"}, status=status.HTTP_400_BAD_REQUEST)
+
     for q in request.data['questions']:
         if not 'id' in q.keys():
             try:
-                question = Question.add(question=q['question'], topic=q['topic'], type=q['type'],
-                    level=q['level'], visibility=q['visibility'], userID=request.user.username)
-                if not question == None:
-                    InterviewQuestion.add(interview, question)
-                else:
-                    InterviewQuestion.add(interview, Question.objects.get(question=q['question']))
+                if 'answers' in q.keys():
+                    question = Question.add(question=q['question'], topic=q['topic'], type=q['type'],
+                                            level=q['level'], visibility=q['visibility'], userID=request.user.username)
+                    if not question == None:
+                        InterviewQuestion.add(interview, question)
+                        answer = QuestionAnswers.add(question=question, answers=q['answers'])
+
+                    else:
+                        InterviewQuestion.add(interview, Question.objects.get(question=q['question']))
             except Exception as Err:
-                return Response({"Error 500" : "Internal Server Error", "detail":str(Err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                return Response({"Error 500": "Internal Server Error", "detail": str(Err)},
+                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         else:
             try:
                 InterviewQuestion.add(interview, Question.objects.get(id=q['id']))
             except:
-                return Response({"Error 404" : "Question Not Found", "detail":"Can't Find Question"}, status=status.HTTP_404_NOT_FOUND)
-    return Response({"OK 200":"Interview Added Successfully"}, status=status.HTTP_200_OK)
+                return Response({"Error 404": "Question Not Found", "detail": "Can't Find Question"},
+                                status=status.HTTP_404_NOT_FOUND)
+    return Response({"OK 200": "Interview Added Successfully"}, status=status.HTTP_200_OK)
+
 
 @api_view(['POST'])
 @IsAuthenticated
@@ -208,18 +224,29 @@ def AddInterview(request):
 def EditInterview(request):
     pass
 
+
 @api_view(['POST'])
 @IsAuthenticated
 @CanCreateQuestion
 def AddQuestion(request):
     try:
-        question = Question.add(question=request.data['question'], topic=request.data['topic'], type=request.data['type'],
-            level=request.data['level'], visibility=request.data['visibility'], userID=request.user.username)
+        if request.data['answers']:
+            question = Question.add(question=request.data['question'], topic=request.data['topic'],
+                                    type=request.data['type'],
+                                    level=request.data['level'], visibility=request.data['visibility'],
+                                    userID=request.user.username)
+            answer = QuestionAnswers.add(question=question, answers=request.data['answers'])
+
         if not question:
-            return Response({"Error 409" : "Question Already Exist", "detail":"Can't Add Question because it's already Exist"}, status=status.HTTP_409_CONFLICT)
+            return Response(
+                {"Error 409": "Question Already Exist", "detail": "Can't Add Question because it's already Exist"},
+                status=status.HTTP_409_CONFLICT)
     except Exception as err:
-        return Response({"Error 400" : "Bad Request", "detail":"Can't Add Question with provided data", "more":str(err)}, status=status.HTTP_400_BAD_REQUEST)
-    return Response({"OK 200":"Success"}, status=status.HTTP_200_OK)
+        return Response(
+            {"Error 400": "Bad Request", "detail": "Can't Add Question with provided data", "more": str(err)},
+            status=status.HTTP_400_BAD_REQUEST)
+    return Response({"OK 200": "Success"}, status=status.HTTP_200_OK)
+
 
 @api_view(['POST'])
 @IsAuthenticated
@@ -233,8 +260,9 @@ def EditQuestion(request):
 @CanCreateInterview
 def AddInterviewsTopic(request):
     if Topic.objects.filter(topicName=request.data['name']).exists():
-        return Response({"Error 409":"Database Conflict", "detail" :"Topic Already Exists"}, status=status.HTTP_409_CONFLICT)
+        return Response({"Error 409": "Database Conflict", "detail": "Topic Already Exists"},
+                        status=status.HTTP_409_CONFLICT)
     else:
         topic = Topic(topicName=request.data['name'])
         topic.save()
-    return Response({"Ok 200":"Topic Added Successfully"}, status=status.HTTP_200_OK)
+    return Response({"Ok 200": "Topic Added Successfully"}, status=status.HTTP_200_OK)
--- source/server/models.py
+++ source/server/models.py
@@ -5,9 +5,11 @@ from datetime import timedelta
 
 from server.permissions import *
 
+
 # Account Types
 class AccountType(models.Model):
     typeTitle = models.CharField(max_length=50)
+
     def __str__(self):
         return self.typeTitle
 
@@ -19,7 +21,7 @@ class User(AbstractUser):
     accountType = models.ForeignKey(AccountType, on_delete=models.CASCADE, null=True)
     email = models.EmailField(unique=True)
     lastModified = models.DateField(auto_now=True)
-    
+
     class Meta:
         permissions = [
             (CAN_CREATE_QUESTION, 'Can Create A Question'),
@@ -34,46 +36,47 @@ class User(AbstractUser):
     def register(username, password, email, first_name, last_name, gender, dateOfBirth, accountType=None):
         if gender not in ['M', 'F']:
             raise Exception("Invalid Gender")
-        if(accountType):
+        if (accountType):
             try:
                 account = AccountType.objects.filter(typeTitle=accountType)[0]
             except:
                 raise Exception("Invalid Account Type")
         else:
             account = AccountType.objects.filter(typeTitle='Interviewee')[0]
-        if(len(username)<8):
+        if (len(username) < 8):
             raise Exception("Username Must Be At Least 8 Characters")
-        if(len(email) < 4):
+        if (len(email) < 4):
             raise Exception("Incorrect Email Address")
-        
+
         username = User.normalize_username(username)
         email = str(email).lower()
         if User.objects.filter(username=username).exists():
             raise Exception("Account Username Already Exists")
-        
+
         if User.objects.filter(email=email).exists():
             raise Exception("Account Email Address Already Exists")
 
         birthDate = datetime.datetime.strptime(str(dateOfBirth), '%d-%m-%Y')
         print(birthDate)
         user = User(username=username,
-        email = email,
-        first_name=first_name,
-        last_name=last_name,
-        gender=gender,
-        dateOfBirth= birthDate,
-        accountType = account)
-        
-        
+                    email=email,
+                    first_name=first_name,
+                    last_name=last_name,
+                    gender=gender,
+                    dateOfBirth=birthDate,
+                    accountType=account)
+
         user.set_password(password)
         user.save()
         user.groups.add(Group.objects.get(name='Default'))
         return user
-    def AssignToGroup(self,groupName):
+
+    def AssignToGroup(self, groupName):
         try:
             self.groups.add(Group.objects.get(name=groupName))
         except:
             raise Exception("Invalid Group Name")
+
     def edit(self, username=None, password=None, email=None, first_name=None, last_name=None, accountType=None):
         if not username == None:
             if User.objects.filter(username=username).exists():
@@ -91,12 +94,12 @@ class User(AbstractUser):
                 self.set_password(password)
             else:
                 raise Exception("Password Should Be 8 or more Characters")
-        
+
         if not first_name == None:
             self.first_name = first_name
         if not last_name == None:
             self.last_name = last_name
-        
+
         if not accountType == None:
             try:
                 account = AccountType.objects.filter(typeTitle=accountType)[0]
@@ -106,8 +109,11 @@ class User(AbstractUser):
             except:
                 raise Exception("Invalid Account Type")
         self.save()
+
+
 class Topic(models.Model):
     topicName = models.CharField(max_length=100)
+
     def __str__(self):
         return self.topicName
 
@@ -117,10 +123,14 @@ class Interview(models.Model):
     title = models.CharField(max_length=200)
     duration = models.IntegerField(default=2)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
+
     def add(userID, title, duration, topic, *args, **kwargs):
-        interview = Interview(userID = userID, title=title, duration = duration, topic=Topic.objects.get(topicName= topic))
+        if Interview.objects.filter(title=title).exists():
+            return None
+        interview = Interview(userID=userID, title=title, duration=duration, topic=Topic.objects.get(topicName=topic))
         interview.save()
         return interview
 
@@ -132,47 +142,67 @@ class Question(models.Model):
     level = models.CharField(max_length=15)
     visibility = models.CharField(max_length=15)
     userID = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.question
+
     def add(question, topic, type, level, visibility, userID):
         if Question.objects.filter(question=question).exists():
             return None
         else:
             try:
-                question = Question(question=question, topic=topic, type=type, level=level, visibility=visibility, userID=User.objects.get(username=userID))
+                question = Question(question=question, topic=topic, type=type, level=level, visibility=visibility,
+                                    userID=User.objects.get(username=userID))
                 question.save()
             except Exception as err:
                 raise err
         return question
-            
+
+
 class QuestionAnswers(models.Model):
     questionID = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer = models.CharField(max_length=255)
+
+    def add(question, answers):
+        try:
+            for answer in answers:
+                questionAnswer = QuestionAnswers(questionID=Question.objects.get(question=question), answer=answer)
+                questionAnswer.save()
+        except Exception as err:
+            raise err
+        return questionAnswer
 
 
 class InterviewQuestion(models.Model):
     questionID = models.ForeignKey(Question, on_delete=models.CASCADE)
     interviewID = models.ForeignKey(Interview, on_delete=models.CASCADE)
+
     def add(i, q):
-        iq = InterviewQuestion(questionID=q,interviewID= i)
+        iq = InterviewQuestion(questionID=q, interviewID=i)
         iq.save()
+
+
 # Report
 class Report(models.Model):
     score = models.IntegerField()
     summary = models.CharField(max_length=255)
-    
+
 
 class ReportWeakness(models.Model):
     reportID = models.ForeignKey(Report, on_delete=models.CASCADE)
     weakness = models.CharField(max_length=255)
+
     def __str__(self):
         return self.weakness
+
 
 class ReportStrengths(models.Model):
     reportID = models.ForeignKey(Report, on_delete=models.CASCADE)
     strengths = models.CharField(max_length=255)
+
     def __str__(self):
         return self.strengths
+
 
 class InterviewAttendance(models.Model):
     reportID = models.ForeignKey(Report, on_delete=models.CASCADE)
@@ -181,11 +211,13 @@ class InterviewAttendance(models.Model):
     attendanceDate = models.DateTimeField(auto_now_add=True)
     duration = models.DurationField()
 
+
 class yp6AuthenticationToken(models.Model):
     userID = models.ForeignKey(User, on_delete=models.CASCADE)
     macAddress = models.TextField(null=False, default="0.0.0.0", max_length=20)
     optainedTime = models.DateTimeField(auto_now_add=True)
     token = models.TextField(null=False, default="None", max_length=64)
-    expiry = models.DateTimeField(default= (datetime.datetime.now() + datetime.timedelta(days=7)))
+    expiry = models.DateTimeField(default=(datetime.datetime.now() + datetime.timedelta(days=7)))
+
     def __str__(self):
         return self.macAddress
